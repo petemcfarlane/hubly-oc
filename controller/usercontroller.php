2bf381fb0089ed3d3e22efdf5189a8a9f67e62a3
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2013-07-31 16:10:41 +0100

    fixed signup and login

--- controller/usercontroller.php
+++ controller/usercontroller.php
@@ -4,6 +4,8 @@ namespace OCA\Hubly\Controller;
 use \OCA\AppFramework\Controller\Controller;
 use \OCA\AppFramework\Core\API;
 use \OCA\AppFramework\Http\Request;
+use \OCA\AppFramework\Http\JSONResponse;
+use \OCA\AppFramework\Http\TemplateResponse;
 
 use \OCA\Hubly\Controller\PageController;
 use \OCP\User;
@@ -18,9 +20,34 @@ class UserController extends Controller {
 
 	public function __construct(API $api, Request $request){
 		parent::__construct($api, $request);
-		$this->user = new User;
+		$this->user = new User; // used by pagecontroller
 	}
 	
+	protected function redirect($url='hubly.page.index') {
+		$response = new TemplateResponse($this->api, 'guest_index');
+		$response->addHeader('Location', \OCP\Util::linkToRoute($url) );
+		return $response;
+	}
+
+	/**
+	 * @IsAdminExemption
+	 * @IsSubAdminExemption
+	 * @CSRFExemption
+	 * @IsLoggedInExemption
+	 */
+	public function login() {
+		try {
+			if ( empty($this->request->email) )    throw new Exception('Email address must be provided', 1);
+			if ( empty($this->request->password) ) throw new Exception('Password must be provided', 2);	
+			if ( !\OC_User::login($this->request->email, $this->request->password) ) 
+						                           throw new Exception('Email address or password incorrect. Please try again', 2);
+			return $this->redirect();
+		} catch(Exception $exception) {
+			$params['response'] = array( 'status'=>'error', 'message'=>$exception->getMessage(), 'code'=>$exception->getCode() );
+			$params['args'] = array("email" => $this->request->email);
+			return $this->render('login', $params, '');
+		}
+	}
 	
 	/**
 	 * @IsAdminExemption
@@ -28,38 +55,44 @@ class UserController extends Controller {
 	 * @CSRFExemption
 	 * @IsLoggedInExemption
 	 */
-	public function signup($groups=array('hubly'), $quota="100 MB"){
-		try { // try to signup, if successfull, login and go to home page
-			$this->displayName = $_POST['name'];
-			$this->uid = $_POST['email'];
-			$this->uid_confirmation = $_POST['email_confirmation'];
-			$this->password = $_POST['password'];
-			if (!$this->displayName) throw new Exception('Please provide a display name so we know what to call you', 1);	
-			if (!$this->uid) throw new Exception('Email address must be provided, we promise not to spam you', 2);
-			if (!$this->uid_confirmation) throw new Exception('Please re-enter your email to make sure you got it right', 3);
-			if ($this->uid !== $this->uid_confirmation) throw new Exception('The Email addresses you entered didn\'t match', 4);
-			if (strlen($this->password)<6) throw new Exception('Please choose a password of at least 6 characters', 5);
-			
-			$this->user->createUser($this->uid, $this->password);
-			$this->user->setDisplayName($this->uid, $this->displayName);
-			\OC_Preferences::setValue($this->uid, 'files', 'quota', $quota);
+	public function logout() {
+		$this->user->logout();
+		return $this->redirect();
+	}
+	
+	/**
+	 * @IsAdminExemption
+	 * @IsSubAdminExemption
+	 * @CSRFExemption
+	 * @IsLoggedInExemption
+	 */
+	public function signup($groups=array('hubly'), $quota="100 MB") {
+		try {
+			if ( empty($this->request->name) ) throw new Exception('Please provide a display name so we know what to call you', 1);	
+			if ( empty($this->request->email)) throw new Exception('Email address must be provided, we promise not to spam you', 2);
+			if ( empty($this->request->email_confirmation) ) 
+												throw new Exception('Please re-enter your email to make sure you got it right', 3);
+			if ( $this->request->email !== $this->request->email_confirmation ) 
+												throw new Exception('The Email addresses you entered didn\'t match', 4);
+			if ( strlen($this->request->password) < 6 ) throw new Exception('Please choose a password of at least 6 characters', 5);
+			if ( !\OC_User::createUser($this->request->email, $this->request->password) ) throw new Exception('User creation failed');
+			\OC_User::setDisplayName($this->request->email, $this->request->name);
 			foreach( $groups as $i ) {
 				if(!\OC_Group::groupExists($i)) {
 					\OC_Group::createGroup($i);
 				}
-				\OC_Group::addToGroup( $this->uid, $i );
+				\OC_Group::addToGroup( $this->request->email, $i );
 			}
-			$user->login($this->uid, $this->password);
-			$location = \OCP\Util::linkToRoute("hubly.page.index");
-			header( 'Location: '.$location );
+			if ( !\OC_User::login($this->request->email, $this->request->password) ) throw new Exception('Login failed');
+			return $this->redirect('hubly.page.index');
 		} catch (Exception $exception) {
 			if ($exception->getMessage() == 'The username is already being used') {
 				$params['response'] = array( 'status' => 'error', 'message' => "The email address has already been used", 'code' => 6 );
 			} else {
 				$params['response'] = array( 'status' => 'error', 'message' => $exception->getMessage(), 'code' => $exception->getCode() );
 			}
-			$params['args'] = array("name" => $this->displayName, "email" => $this->uid);
+			$params['args'] = array("name" => $this->request->name, "email" => $this->request->email);
 			return $this->render('signup', $params, '');
 		}
-	}
+	}		
 }
--- templates/login.php
+++ templates/login.php
@@ -4,12 +4,11 @@
 <div class="row">
 	<div class="large-6 columns large-offset-3">
 		<h1>Login</h1>
-		<form method="post" action="/">
-			<input type="hidden" name="redirect_url" value="{{ url("hubly.page.index") }}" />
+		<form method="post" action="{{ url("hubly.user.login") }}">
 			<label for="login_email">Your email</label>
-			<input id="login_email" type="text" name="user" placeholder="Email"/>
+			<input id="login_email" type="text" name="email" placeholder="Email" {% if response.code == 1 %}class="error"{% endif %}{% if args.email %}value="{{ args.email }}" {% endif %}/>
 			<label for="login_password">Your password</label>
-			<input id="login_password" type="password" name="password" placeholder="Password" />
+			<input id="login_password" type="password" name="password" placeholder="Password" {% if response.code == 2 %}class="error"{% endif %}/>
 			<input type="submit" value="Login" class="button" />
 		</form>
 		<p><a href="{{ url('hubly.page.signup') }}">Click here if you need to sign up</a></p>
